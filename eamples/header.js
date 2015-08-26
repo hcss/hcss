b863aeb8667ecd6796dc2bea0ce7df85b7cd364c
georgebbbb <510223397@qq.com>
2015-08-26 23:18:15 +0800

    just try

---
+++ eamples/header.js
@@ -0,0 +1,1 @@
+var $=require("../src/hcss.js")
---
+++ src/hcss.js
@@ -0,0 +1,24 @@
+module.exports={
+	dom:function(){
+		return new Dom(arguments)
+	},
+	div:function(){
+		return new Div(arguments)
+	},
+	span:function(){
+		return new Span(arguments)
+	}
+}
+
+Dom:function(){
+
+	},
+Div:function(){
+
+	},
+P:function(){
+
+	},
+Span:function(){
+		
+	}	
